252e48a23b0bbc5cf753bec528d24827c256af19
unknown <zuoweitaoting@gmail.com>
2020-08-12 18:19:38 +0800

    some modify

---
+++ logtest.cmd
@@ -0,0 +1,1 @@
+.\venv\Scripts\python.exe .\src\test\test.py
--- src/view/mainview.py
+++ src/view/mainview.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 import logging
 
-import imgui, glfw
+import glfw, imgui
 
 from src.common import utils
 from src.model.mainconfig import MainConfig, EConfigKey
